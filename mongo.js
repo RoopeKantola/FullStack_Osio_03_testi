929e7eaab4db025fc4d6c4e33958a2ca231b8a21
roope <kantolar@uef.fi>
2023-09-19 12:52:18 +0300

    uibuild

--- mongo.js
+++ mongo.js
@@ -8,24 +8,26 @@ if (process.argv.length<3) {
 const password = process.argv[2]
 
 const url =
-`mongodb+srv://fullstack:${password}@cluster0.rrdnx4e.mongodb.net/noteApp?retryWrites=true&w=majority`
+`mongodb+srv://fullstack:${password}@cluster0.rrdnx4e.mongodb.net/testNoteApp?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
   content: String,
+  date: Date,
   important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: 'HTML is Easy',
+  content: 'Mongoose testing no.2 in testDatabase',
+  date: new Date(),
   important: true,
 })
-
-/*note.save().then(result => {
+/*
+note.save().then(result => {
   console.log('note saved!')
   mongoose.connection.close()
 })*/
